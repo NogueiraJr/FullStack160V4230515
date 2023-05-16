71cd4298885de56263dc53ec6464ee5d54df29c7
Nogueira, Admilson M Jr <nogueirajr@hotmail.com>
2023-05-16 10:25:09 -0300

    Change to Service

--- server.js
+++ server.js
@@ -1,4 +1,37 @@
+const express = require('express');
 const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+// Rota para obter informações de geocodificação e previsão do tempo
+app.get('/weather-forecast', async (req, res) => {
+    const address = req.query.address;
+
+    try {
+        // Obter dados de geocodificação
+        const geocodingData = await getGeocodingData(address);
+
+        if (Array.isArray(geocodingData) && geocodingData.length > 0) {
+            // Extrair latitude e longitude do primeiro resultado de geocodificação
+            const latitude = geocodingData[0].lat;
+            const longitude = geocodingData[0].lon;
+
+            // Obter previsão do tempo
+            const PrevisaoDoTempo = await getWeatherForecast(latitude, longitude);
+
+            res.json({
+                geocodingData: geocodingData,
+                PrevisaoDoTempo: PrevisaoDoTempo
+            });
+        } else {
+            res.status(404).json({ error: 'Nenhum resultado de geocodificação encontrado.' });
+        }
+    } catch (error) {
+        console.error('Ocorreu um erro:', error);
+        res.status(500).json({ error: 'Erro ao processar a solicitação.' });
+    }
+});
 
 // Função para obter informações de geocodificação
 async function getGeocodingData(address) {
@@ -22,28 +55,7 @@ async function getWeatherForecast(latitude, longitude) {
     }
 }
 
-// Exemplo de uso das funções
-async function main() {
-    try {
-        // Obter dados de geocodificação
-        const geocodingData = await getGeocodingData('San Francisco, CA');
-
-        if (Array.isArray(geocodingData) && geocodingData.length > 0) {
-            // Extrair latitude e longitude do primeiro resultado de geocodificação
-            const latitude = geocodingData[0].lat;
-            const longitude = geocodingData[0].lon;
-
-            // Obter previsão do tempo
-            const weatherForecast = await getWeatherForecast(latitude, longitude);
-
-            //console.log('DadosDeGeocodificacao:', geocodingData);
-            console.log('PrevisaoDoTempo:', weatherForecast);
-        } else {
-            console.error('Nenhum resultado de geocodificação encontrado.');
-        }
-    } catch (error) {
-        console.error('Ocorreu um erro:', error);
-    }
-}
-
-main();
+// Iniciar o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
